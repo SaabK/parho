ab63babc55bf916f30521e46afa6a179b680ff9b
Ali Bin Naseer <aliisinsane0089@gmail.com>
2024-01-02 09:55:28 +0500

    Basic Setup

---
+++ app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Hello, World</h1>
+            <Button>Hey</Button>
+        </>
+    );
+}
