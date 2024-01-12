15750df86c25d73a008d317aea67e828a82d1b70
Ali Bin Naseer <aliisinsane0089@gmail.com>
2024-01-12 15:58:22 +0500

    Added Sale Products Route

---
+++ app/api/books/sale/route.ts
@@ -0,0 +1,20 @@
+import { connectToMongo } from "@/lib/db";
+import Book from "@/models/Book";
+import { NextResponse } from "next/server";
+
+connectToMongo();
+
+export async function GET() {
+    const books = await Book.find().skip(20).limit(7);
+    console.log(books.length);
+
+    return NextResponse.json(
+        {
+            message: "Successfully Fetched the Books",
+            books,
+        },
+        {
+            status: 200,
+        }
+    );
+}
